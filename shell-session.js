f3d3d307218d309e4b5e2ab1e4b5ad27aafc30ab
Erik Luu <63026434+erikluu@users.noreply.github.com>
2022-04-06 22:25:44 -0700

    added bullet point

--- shell-session.js
+++ shell-session.js
@@ -1,7 +1,7 @@
 // setup typewriter effect in the terminal
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = "start ...\n\n###a little about me\n\n- Studying Computer Science @ Cal Poly, San Luis Obispo\n- NCAA D1 Cross Country & Track Athlete\n- From Seattle, WA";
+  var txt = `start ...\n\n###a little about me\n\n- Studying Computer Science @ Cal Poly, San Luis Obispo\n- NCAA D1 Cross Country & Track Athlete\n- Research Assistant, AI for Search and Rescue\n- From Seattle, WA`;
   var speed = 30;
 
   function typeTxtOut () {
