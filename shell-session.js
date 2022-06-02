e136ac81cda33046b93d47e8305aa03b285b0e33
Erik Luu <63026434+erikluu@users.noreply.github.com>
2022-06-01 21:45:24 -0700

    Update shell-session.js

--- shell-session.js
+++ shell-session.js
@@ -2,7 +2,7 @@
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
   var txt = `start ...\n\n###a little about me\n\n- Studying Computer Science @ Cal Poly, San Luis Obispo\n- NCAA D1 Cross Country & Track Athlete\n- Research Assistant, AI for Search and Rescue\n- Research Assistant, Damage Map\n- From Seattle, WA`;
-  var speed = 30;
+  var speed = 40;
 
   function typeTxtOut () {
     if (i < txt.length) {
