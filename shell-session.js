b5e392f026e1bd054101cb3abfc256ea2d6a56ab
Erik Luu <63026434+erikluu@users.noreply.github.com>
2024-01-27 12:48:26 -0800

    Update shell-session.js

--- shell-session.js
+++ shell-session.js
@@ -1,7 +1,7 @@
 // setup typewriter effect in the terminal
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `describe --me\n\n###a little about me\n\n- M.S. Computer Science @ Cal Poly, San Luis Obispo\n- NCAA D1 Cross Country & Track Athlete\n- Software Engineer Intern for Cylerian LLC\n- From Seattle, WA\n\n tags: Machine Learning; Data Science; Accessibility; Knowledge Graphs`;
+  var txt = `describe --me\n\n###a little about me\n\n- M.S. Computer Science @ Cal Poly, San Luis Obispo\n- NCAA D1 Cross Country & Track Athlete\n- Software Engineer Intern for Cylerian LLC\n- From Seattle, WA\n\ntags: Machine Learning; Data Science; Accessibility; Knowledge Graphs`;
   var speed = 5;
 
   function typeTxtOut () {
